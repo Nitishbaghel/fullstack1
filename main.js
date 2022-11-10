9e744f27e334a74779de3765b060e74e9a2862fc
Nitishbaghel <nitishbaghel001@gmail.com>
2022-11-10 10:55:45 +0530

    ff

--- main.js
+++ main.js
@@ -1,9 +1,121 @@
 
 
-console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.body);
-console.log(document.head);
-console.log(document.link);
-console.log(document.all);
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.link);
+// console.log(document.all[5]);
+
+// document.all[5].textContent='hello world';
+
+// document.log(document.all[12].innertext);
+
+
+//quering document
+let header=document.getElementById('header-title');
+header.style.backgroundColor='red'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
