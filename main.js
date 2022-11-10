660e6ae31d577450b71ac8518c8d0e0297966269
Nitishbaghel <nitishbaghel001@gmail.com>
2022-11-10 10:40:37 +0530

    hh

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.body);
+console.log(document.head);
+console.log(document.link);
+console.log(document.all);
