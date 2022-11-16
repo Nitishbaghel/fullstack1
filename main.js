021bca4a6057c74a0349853da4f6feb1fc7db4e8
Nitishbaghel <nitishbaghel001@gmail.com>
2022-11-16 09:44:20 +0530

    changes

--- main.js
+++ main.js
@@ -14,16 +14,36 @@
 
 
 //quering document
-let header=document.getElementById('header-title');
-header.style.backgroundColor='red'
+// let header=document.getElementById('header-title');
+// header.style.backgroundColor='red';
+// header.innerText='ind'
+// header.innerHTML="<h3>jhdijf</h3>"
+
+// console.log(header)
+
+// var item =document.getElementById("list-group-item");
+// console.log.apply(item);
+// for(let i=0;i<item.length;i++){
+//     Object.assign(item[i].style,{backgroundColor:'red',fontWeight:'bold'})
+// }
+
+/* traverse dom*/
+ 
+var itemlist =document.getElementById('items');
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor='lightgrey';
+// console.log(itemlist.parentElement);
+
+// console.log(itemlist.childNodes)
+// console.log(itemlist.children);
 
 
+// itemlist.children[1].style.backgroundColor='yellow';
+// console.log(itemlist.children[1])
 
-
-
-
-
-
+function buttonclick(){
+    console.log('hello world');
+}
 
 
 
